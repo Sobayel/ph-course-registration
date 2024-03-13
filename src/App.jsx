518baffb1242520cf8eb89d2cf7f73c1faa3bd97
mdsobayelhossain <mdsobayelhossain@gmail.com>
2024-03-13 16:22:11 +0600

    setup this all function

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold underline text-primary">
+      PH Course Registration</h1>
     </>
   )
 }
